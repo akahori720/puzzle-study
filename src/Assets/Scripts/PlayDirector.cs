6c3035fc3e35e3b38bd47557f6973cb5cf96079c
akahori720 <ayappy2001@gmail.com>
2023-07-21 11:50:36 +0900

    課題１０まで完了済み。

--- src/Assets/Scripts/PlayDirector.cs
+++ src/Assets/Scripts/PlayDirector.cs
@@ -1,6 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+interface IState
+{
+    public enum E_State
+    {
+        Control = 0,
+        GameOver = 1,
+
+        MAX,
+
+        Unchanged,
+    }
+
+    E_State Initialize(PlayDirector parent);
+    E_State Update(PlayDirector parent);
+}
 
 public class PlayDirector : MonoBehaviour
 {
@@ -11,6 +28,14 @@ public class PlayDirector : MonoBehaviour
     NextQueue _nextQueue = new();
     [SerializeField] PuyoPair[] nextPuyoPairs = { default!, default! };
 
+
+
+    IState.E_State _current_state = IState.E_State.Control;
+    static readonly IState[] states = new IState[(int)IState.E_State.MAX]{
+        new ControlState(),
+        new GameOverState(),
+    };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +44,8 @@ public class PlayDirector : MonoBehaviour
         _playerController.SetLogicalInput(_logicalInput);
 
         _nextQueue.Initialize();
-        Spawn(_nextQueue.Update());
-        UpdateNextsView();
+
+        InitializeState();
     }
 
     void UpdateNextsView()
@@ -30,7 +55,6 @@ public class PlayDirector : MonoBehaviour
             nextPuyoPairs[idx++].SetPuyoType((PuyoType)n.x, (PuyoType)n.y);
         });
     }
- 
 
     static readonly KeyCode[] key_code_tbl = new KeyCode[(int)LogicalInput.Key.MAX]{
         KeyCode.RightArrow,
@@ -41,11 +65,9 @@ public class PlayDirector : MonoBehaviour
         KeyCode.DownArrow,
     };
 
-
     void UpdateInput()
     {
         LogicalInput.Key inputDev = 0;
-
 
         for (int i = 0; i < (int)LogicalInput.Key.MAX; i++)
         {
@@ -54,22 +76,67 @@ public class PlayDirector : MonoBehaviour
                 inputDev |= (LogicalInput.Key)(1 << i);
             }
         }
-
         _logicalInput.Update(inputDev);
     }
 
+    class ControlState : IState
+    {
+        public IState.E_State Initialize(PlayDirector parent)
+        {
+            if (!parent.Spawn(parent._nextQueue.Update())){
+                return IState.E_State.GameOver;
+            }
 
+            parent.UpdateNextsView();
+            return IState.E_State.Unchanged;
+        }
+        public IState.E_State Update(PlayDirector parent) {
+            return parent.player.activeSelf ?IState.E_State.Unchanged : IState.E_State.Control;
+        }
+    }
+
+    class GameOverState : IState
+    {
+        public IState.E_State Initialize(PlayDirector parent) {
+            SceneManager.LoadScene(0);
+            return IState.E_State.Unchanged;
+        }
+        public IState.E_State Update(PlayDirector parent) { return IState.E_State.Unchanged; }
+    }
+
+    void InitializeState()
+    {
+        Debug.Assert(condition: _current_state is >= 0 and < IState.E_State.MAX);
+
+        var next_state = states[(int)_current_state].Initialize(this);
+
+        if (next_state != IState.E_State.Unchanged)
+        {
+            _current_state = next_state;
+            InitializeState();
+        }
+
+    }
+
+    void UpdateState()
+    {
+        Debug.Assert(condition: _current_state is >= 0 and < IState.E_State.MAX);
+
+        var next_state = states[(int)_current_state].Update(this);
+        if (next_state != IState.E_State.Unchanged)
+        {
+
+            _current_state = next_state;
+            InitializeState();
+        }
+    }
+
+    bool Spawn(Vector2Int next) => _playerController.Spawn((PuyoType)next[0], (PuyoType)next[1]);
 
     void FixedUpdate()
     {
-
         UpdateInput();
 
-        if (!player.activeSelf)
-        {
-            Spawn(_nextQueue.Update());
-            UpdateNextsView();
-        }
+        UpdateState();
     }
-    bool Spawn(Vector2Int next) => _playerController.Spawn((PuyoType)next[0], (PuyoType)next[1]);
 }
